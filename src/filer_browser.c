aa9651f7a0099ccc38eb38bf9e142c55c40afc3a
R3Z3N / Saildot4K <oajapplefan@gmail.com>
2026-09-14 09:35:44 -0700

    Icon.sys fix (#51)
    * Update makeicon.c
    
    * allow title.cfg creation on all devices
    
    * Edit multiple timestamps before saving changes allowed
    
    * Timestamp auto organize (#50)
    
    * timestamp auto organize feature
    
    * protect tuna folder timestamps, progress bar

--- src/filer_browser.c
+++ src/filer_browser.c
@@ -314,11 +314,16 @@ static int isTitleCfgPathEligible(const char *path, int menu_disabled)
 {
 	return ((!strncmp(path, "mass", 4)) ||
 	        (!strncmp(path, "usb", 3)) ||
+	        (!strncmp(path, "mc0:", 4)) ||
+	        (!strncmp(path, "mc1:", 4)) ||
 #ifdef MMCE
 	        (!strncmp(path, "mmce", 4)) ||
 #endif
 #ifdef MX4SIO
 	        (!strncmp(path, "mx4sio", 6)) ||
+#endif
+#ifdef UDPFS
+	        (!strncmp(path, "udpfs", 5)) ||
 #endif
 	        (!strncmp(path, "ata", 3)) ||
 	        (isHddBrowserPath(path) && !menu_disabled));
@@ -445,7 +450,7 @@ static int menu(const char *path, FILEINFO *file)
 	} else {
 		enable[TIMEMANIP] = FALSE;
 	}
-	enable[TIMEMANIP_CUSTOM] = filerCanSetCustomTimestamp(path, file);
+	enable[TIMEMANIP_CUSTOM] = filerCanOrganizeFolderTimestamps(path);
 //#endif //TMANIP
 	if (genCmpFileExt(file->name, "ELF") && isTitleCfgPathEligible(path, menu_disabled))
 		enable[TITLE_CFG] = TRUE;
@@ -1534,27 +1539,18 @@ int getFilePath(char *out, int cnfmode)
 					}  //ends GETSIZE
 //#ifdef TMANIP
 					else if (ret == TIMEMANIP) {
-#ifdef TMANIP_MORON
-						sprintf(msg1, "\n\n %s  [%s]  ?\n", LNG(change_timestamp_of), HACK_FOLDER);
-#else
-						sprintf(msg1, "\n\n %s  [%s]  ?\n", LNG(change_timestamp_of), files[browser_sel].name);
-#endif //TMANIP_MORON
-						if (filerConfirmExploitModify(path, &files[browser_sel]) > 0 && ynDialog(msg1) > 0) {
-							time_manip(path, &files[browser_sel], msg0);
-							browser_pushed = FALSE;
-							browser_repos = TRUE;  // TEST
-							browser_cd = TRUE;     //TEST
-						}
+						time_manip(path, &files[browser_sel], msg0);
+						browser_pushed = FALSE;
+						browser_repos = TRUE;  // TEST
+						browser_cd = TRUE;     //TEST
 					}
 					else if (ret == TIMEMANIP_CUSTOM) {
-						if (filerConfirmExploitModify(path, &files[browser_sel]) > 0) {
-							ret = time_manip_custom(path, &files[browser_sel], msg0);
-							if (ret != 0)
-								browser_pushed = FALSE;
-							if (ret > 0) {
-								browser_repos = TRUE;
-								browser_cd = TRUE;
-							}
+						ret = time_manip_organize(path, &files[browser_sel], msg0);
+						if (ret != 0)
+							browser_pushed = FALSE;
+						if (ret > 0) {
+							browser_repos = TRUE;
+							browser_cd = TRUE;
 						}
 					}
 
